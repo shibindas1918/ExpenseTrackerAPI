4fec5a978ac51d6854e2716d825aad94eb3b9e93
sibin <sibindas1806@gmail.com>
2024-11-28 14:21:30 +0530

    changes done

--- ExpenseTrackerAPI/Program.cs
+++ ExpenseTrackerAPI/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton(new DatabaseHelper(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSingleton(new DatabaseHelper(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 
 var app = builder.Build();
